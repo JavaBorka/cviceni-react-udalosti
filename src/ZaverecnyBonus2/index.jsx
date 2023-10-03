352829e25fa39d8cab785d4dd2c693326ca01998
JavaBorka <borka34@gmail.com>
2023-10-03 13:38:24 +0200

    Závěrečný Bonus 2 + komentář od Luďka

--- src/ZaverecnyBonus2/index.jsx
+++ src/ZaverecnyBonus2/index.jsx
@@ -24,7 +24,6 @@ import React from 'react'
 
 // Řešení
 
-
 export const ZaverecnyBonus2 = () => {
 
 	return (
@@ -57,4 +56,12 @@ export const ZaverecnyBonus2 = () => {
 			</form>
 		</>
 	)
+
+// Ta metoda confirm vrátí true/false podle toho, zda klikneš na OK nebo Cancel.
+// Ale confirm  je jen typ dialogového okna. Nic se tím fakticky nepotvrzuje. Je to stejné dialogové okno, jako dělá alert() nebo prompt(), jen každé slouží k jinému účelu. Alert jen vyhodí okýnko se zprávou. Prompt ji umožní zadat text. Confirm ti umožní vybrat OK nebo Cancel.
+// Nic se tím skutečně nepotvrzuje, jen ti to vrátí hodnotu true/false a ty podle ní můžeš něco udělat.
+// JavaScriptové události přidané na prvek, mají vždy přednost před výchozími akcemi nastavenými v prohlížeči. Spustí se vždy dřív, a ta výchozí (default) akce se spustí až jako poslední.
+// Výchozí akce pro odkaz, je že prohlížeč načte tu stránku, kam dokaz vede, a zobrazí ji.
+// Když se tady v té komponentě klikne na odkaz, tak se nejdřív spustí ta tvoje onClick událost a vykoná se kód uvnitř. Kdyby na tom prvku těch událostí bylo více, tak by se spustily postupně všechny a nakonec ta výchozí akce → načtení nové stránky. Když kdekoliv v tom řetězci událostí před výchozí akcí zavoláš preventDefault(), tak prohlížeči řekneš, že tu výchozí akci na konci nemá vykonat.
+// Takže pokud se ti z confirm vrátí false, tak zavoláš preventDefault() a prohlížeč neskočí na ten odkaz, na který jsi klikla.
 }
